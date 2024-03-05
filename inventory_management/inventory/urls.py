37575270aaa043c983c2ee3c48384546bcd8f922
marcelo <mrg0809@gmail.com>
2024-03-05 00:18:44 -0600

    signup form, style

---
+++ inventory_management/inventory/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from .views import Index, SignUpView
+
+urlpatterns = [
+    path('', Index.as_view(), name='index'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+]
---
+++ inventory_management/inventory/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView, View
+from django.contrib.auth import authenticate, login
+from .forms import UserRegisterForm
+
+
+# Create your views here.
+class Index(TemplateView):
+    template_name = 'inventory/index.html'
+
+class SignUpView(View):
+    def get(self, request):
+        form = UserRegisterForm()
+        return render(request, 'inventory/signup.html', {'form': form})
+    
+    def post(self, request):
+        form = UserRegisterForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            user = authenticate(
+                username=form.cleaned.data['username'],
+                password=form.cleaned.data['password1']
+            )
+
+            login(request, user)
+            return redirect('index')
+        
+        return render(request, 'inventory/signup.html', {'form': form})
